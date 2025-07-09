ede4dde67176fb3d3839722d2134c37aab3e0921
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2025-07-09 13:51:33 +0600

    Initial setup done

---
+++ src/app/_components/NavBar.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const NavBar = () => {
+    const links =<>
+    <li><Link href='/'>Home</Link></li>
+    <li><Link href='/course'>Course</Link></li>
+    <li><Link href='/about'>About</Link></li>
+    </>
+    return (
+        <div  className="flex items-center justify-between bg-black text-white py-4 px-16">
+            <h1>Title</h1>
+            <ul className="flex items-center gap-6">
+                {links}
+            </ul>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/app/course/page.jsx
@@ -0,0 +1,9 @@
+const page = () => {
+    return (
+        <div>
+            This is course page
+        </div>
+    );
+};
+
+export default page;
