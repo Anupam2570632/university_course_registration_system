a3f4b4dbac214a90037184b525a674c138f02828
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2025-07-23 01:41:08 +0600

    diable susspense error

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,20 +1,18 @@
-"use client";
+'use client'
 
-import { LoginForm } from "@/components/login-form";
+import { LoginForm } from "@/components/login-form"
 import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
 
 export default function Page() {
+
   const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") || "/";
+  const callbackUrl = searchParams.get('callbackUrl') || '/'; 
 
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    (<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <Suspense fallback={<div>Loading login...</div>}>
-          <LoginForm callbackUrl={callbackUrl} />
-        </Suspense>
+        <LoginForm callbackUrl={callbackUrl}/>
       </div>
-    </div>
+    </div>)
   );
 }
